6928d44f3fe0c8ad678857e09360c8ffdbe1327d
Alina <alina.sinyagina@gmail.com>
2022-08-24 14:28:20 -0700

    added link to main page

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = ({ caption }) => {
     return (
         <div className="flex justify-center p4 bg-gray-600">
-            <div className="flex font-semibold text-gray-200">{ caption }</div>
+            <Link to="/">
+                <div
+                    className="flex font-semibold text-gray-200"
+                    id="brand-name"
+                    cursor="pointer"
+                >
+                    { caption }
+                </div>
+            </Link>
         </div>
     )
 }
